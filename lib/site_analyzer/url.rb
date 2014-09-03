2d6efda18f832f687fa2d2f4ea121da0dd6981f7
Daniel Alves <daniel@danielalves.me>
2014-09-03 22:18:06 +0100

    Refactored Url class

--- lib/site_analyzer/url.rb
+++ lib/site_analyzer/url.rb
@@ -3,46 +3,40 @@ module SiteAnalyzer
     require 'uri'
     attr_reader :url, :file_path
     def initialize(url, file_path)
-      @url = normalize_url(url)
+      @url = URI(url)
       @file_path = file_path
     end
 
     def formatted_url
       return "http:#{file_path}" if file_path =~ /\A\/\//
-      return ::File.join(protocol, domain(url), file_path) if file_path =~ /\A\/\w/
-      ::File.join(protocol, clean_url, clean_path)
+      return ::File.join(protocol, domain, file_path) if file_path =~ /\A\/\w/
+      ::File.join(protocol, domain, clean_url_path, clean_path)
     end
 
     private
 
-    def normalize_url(url)
-      url.gsub!(/\/[\w]+\.(html|htm|php|asp|aspx)\z/, '')
-      url.gsub!(/#[\w-]+\z/, '')
-      url
-    end
-
-    def domain(url)
-      URI.parse(url).host
+    def domain
+      url.host
     end
 
     def protocol
-      url.match(/http[s]?:\/\//).to_s || 'http://'
+      "#{url.scheme}://"
     end
 
-    def url_parts
-      url.gsub(/http[s]?:\/\//, '').split('/')
+    def url_path
+      url.path.gsub(/\/[\w]+\.[\w]+\z/, '')
     end
 
     def file_path_parts
       file_path.split('/')
     end
 
-    def clean_url
-      url_parts_clone = url_parts
+    def clean_url_path
+      url_path_clone = url_path.split('/')
       file_path_parts.each do |part|
-        url_parts_clone.pop if part == '..'
+        url_path_clone.pop if part == '..'
       end
-      url_parts_clone.join('/')
+      url_path_clone.join('/')
     end
 
     def clean_path
--- spec/lib/site_analyzer/url_spec.rb
+++ spec/lib/site_analyzer/url_spec.rb
@@ -5,6 +5,8 @@ describe SiteAnalyzer::Url do
   let(:zero_levels) { SiteAnalyzer::Url.new('http://www.example.com/1/2', 'style.css') }
   let(:one_level) { SiteAnalyzer::Url.new('http://www.example.com/1/2', '../style.css') }
   let(:two_levels) { SiteAnalyzer::Url.new('http://www.example.com/1/2', '../../style.css') }
+  let(:complex_url) { SiteAnalyzer::Url.new('https://github.com/alvesdan/site-analyzer/blob/master/lib/site_analyzer/url.rb', 'style.css') }
+  let(:complex_url_two) { SiteAnalyzer::Url.new('https://github.com/alvesdan/site-analyzer/blob/master/lib/site_analyzer/url.rb', '../style.css') }
 
   it 'returns formatted url for zero levels' do
     expect(zero_levels.formatted_url).to eq 'http://www.example.com/1/2/style.css'
@@ -16,5 +18,13 @@ describe SiteAnalyzer::Url do
 
   it 'returns formatted url for two levels' do
     expect(two_levels.formatted_url).to eq 'http://www.example.com/style.css'
+  end
+
+  it 'return formatted url for complex one' do
+    expect(complex_url.formatted_url).to eq 'https://github.com/alvesdan/site-analyzer/blob/master/lib/site_analyzer/style.css'
+  end
+
+  it 'return formatted url for complex one' do
+    expect(complex_url_two.formatted_url).to eq 'https://github.com/alvesdan/site-analyzer/blob/master/lib/style.css'
   end
 end
